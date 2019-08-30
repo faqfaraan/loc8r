5189c0faa5f7bfb1ec5ef9e24086c0c4c6ffa210
Faraan Qureshi <faqfaraan@gmail.com>
2019-08-30 18:54:44 -0400

    second changes

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,20 @@
+// Get 'home' page
+const homeList = (req, res) => {
+    res.render('index', { title: 'Home'});
+};
+
+// Get 'Location info' page
+const locationInfo = (req, res) => {
+    res.render('index', { title: 'Location info' });
+};
+
+// Get 'Add review' page
+const addReview = (req, res) => {
+    res.render('index', { title: 'Add review'});
+};
+
+module.exports = {
+    homeList,
+    locationInfo,
+    addReview
+};
