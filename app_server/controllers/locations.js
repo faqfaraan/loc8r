86b5463c403aab65cc0068ae85a3e7d945e2855e
Faraan <faqfaraan@gmail.com>
2019-08-30 23:42:37 -0400

    remove sense data

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,12 @@
 // Get 'home' page
 const homeList = (req, res) => {
-    res.render('locations-list', { title: 'Home'});
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        }
+    });
 };
 
 // Get 'Location info' page
