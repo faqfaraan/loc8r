53e11970e09ebce68c38c1fc77268080aca30c82
Faraan Qureshi <faqfaraan@gmail.com>
2019-09-01 17:32:05 -0400

    started api work

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-require('./app_server/models/db');
+require('./app_api/models/db');
 
-const index = require('./app_server/routes/index');
-const users = require('./app_server/routes/users');
+const indexRouter = require('./app_server/routes/index');
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
