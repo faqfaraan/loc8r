dbad96f56b83147347f460907770d7e43614ccc2
Faraan <faqfaraan@gmail.com>
2019-09-02 21:22:53 -0400

    started api connection with frontend

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,38 +1,61 @@
-const homeList = (req, res) => {
-    res.render('locations-list',
-      {
+const request = require('request');
+const apiOptions = {
+  server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'https://pacific-sands-48208.herokuapp.com/';
+}
+
+const formatDistance = (distance) => {
+  let thisDistance = 0;
+  let unit = 'm';
+  if (distance > 1000) {
+    thisDistance = parseFloat(distance / 1000).toFixed(1);
+    unit = 'km';
+  } else {
+    thisDistance = Math.floor(distance);
+  }
+  return thisDistance + unit;
+};
+
+const renderHomepage = (req, res, responseBody) => {
+  res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
           title: 'Loc8r',
           strapLine: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [
-          {
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '100m'
-          },
-          {
-            name: 'Cafe Hero',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '200m'
-          },
-          {
-            name: 'Burger Queen',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '250m'
-          }
-        ]
-      }
-    );
+        locations: responseBody
+      },
+      console.log(responseBody)
+  );
+};
+
+const homeList = (req, res) => {
+  const path = '/api/locations';
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {},
+    qs: {
+      lng: -0.7992599,
+      lat: 51.378091,
+      maxDistance: 100
+    }
   };
+  request(
+      requestOptions,
+      (err, response, body) => {
+        let data = [];
+        data = body.map ( (item) => {
+          item.distance = formatDistance(item.distance);
+          return item;
+        });
+        renderHomepage(req, res, data);
+      }
+  );
+};
   
   const locationInfo = (req, res) => {
     res.render('location-info',
